7177c450569acb5ad9a78940450c9d13b3569782
Nesib Fiso <nesibf@hotmail.com>
2024-09-16 10:22:56 +0200

    Create hello_world.py

---
+++ face/hello_world.py
@@ -0,0 +1,24 @@
+# python3 -m pip install face_recognition
+# pip3 install pytube --break-system-packages
+# pip3 install yt_dlp --break-system-packages
+# pip3 install setuptools --break-system-packages
+# pip3 install opencv-python --break-system-packages
+# pip3 install py2app setuptools --break-system-packages
+# pip3 install PyQt6 --break-system-packages
+# pip3 install --upgrade py2app setuptools --break-system-packages
+
+## 1. Create a setup.py file
+# py2applet --make-setup hello_world.py
+# py2applet --make-setup face_detection.py
+
+## 2. Cleanup build directories
+# rm -rf build dist
+
+## 3. Build application with alias mode
+# python3 setup.py py2app -A
+
+## 4. Running application
+# ./dist/hello_world.app/Contents/MacOS/hello_world
+# ./dist/face_detection.app/Contents/MacOS/face_detection
+
+print("Hello, world!")
